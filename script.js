0ad992c3fa9ab0fcd770f13a75e301f40fe843aa
zola107 <camillecolleauxpro123@gmail.com>
2025-12-04 13:27:32 +0000

    finisiosn des reponses vert et rouge

--- script.js
+++ script.js
@@ -10,3 +10,81 @@ if (themeButton1) {
     });
 }
 
+const themeButton2 = document.querySelector('#bouton2');
+
+
+if (themeButton2) {  
+    themeButton2.addEventListener('click', () => {
+        themeButton2.classList.toggle('rouge');
+    });
+}
+
+
+const themeButton3 = document.querySelector('#bouton3');
+
+
+if (themeButton3) {  
+    themeButton3.addEventListener('click', () => {
+        themeButton3.classList.toggle('rouge');
+    });
+}
+
+
+
+const themeButton4 = document.querySelector('#bouton4');
+
+
+if (themeButton4) {  
+    themeButton4.addEventListener('click', () => {
+        themeButton4.classList.toggle('rouge');
+    });
+}
+
+
+const themeButton5 = document.querySelector('#bouton5');
+
+
+if (themeButton5) {  
+    themeButton5.addEventListener('click', () => {
+        themeButton5.classList.toggle('vert');
+    });
+}
+
+
+const themeButton6 = document.querySelector('#bouton6');
+
+
+if (themeButton6) {  
+    themeButton6.addEventListener('click', () => {
+        themeButton6.classList.toggle('rouge');
+    });
+}
+
+
+const themeButton7 = document.querySelector('#bouton7');
+
+
+if (themeButton7) {  
+    themeButton7.addEventListener('click', () => {
+        themeButton7.classList.toggle('rouge');
+    });
+}
+
+
+const themeButton8 = document.querySelector('#bouton8');
+
+
+if (themeButton8) {  
+    themeButton8.addEventListener('click', () => {
+        themeButton8.classList.toggle('rouge');
+    });
+}
+
+const themeButton9 = document.querySelector('#bouton9');
+
+
+if (themeButton9) {  
+    themeButton9.addEventListener('click', () => {
+        themeButton9.classList.toggle('vert');
+    });
+}
